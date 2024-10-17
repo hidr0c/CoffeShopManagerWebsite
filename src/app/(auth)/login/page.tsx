7f32f95a475f6e2b9ba3d715a2d62191ee952cf1
FoxeiZ <foxeiz2004@gmail.com>
2024-10-17 14:59:55 +0700

    update login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -3,26 +3,26 @@ import styles from "./page.module.css";
 
 export default function LoginPage() {
     return (
-        <div>
+        <div className={styles.loginWrapper}>
             <form id={styles.loginForm}>
                 <label>Email</label>
                 <input type="email" placeholder="Nhập email" className={styles.inputField} />
 
-                <label>Password</label>
+                <div className={styles.passwordTitleWrapper}>
+                    <label>Password</label>
+                    <a href="/forgot-password">Quên mật khẩu</a>
+                </div>
                 <input type="password" placeholder="Mật khẩu" className={styles.inputField} />
 
-                <div className={styles.options}>
-                    <div>
-                        <input type="checkbox" id="rememberMe" />
-                        <label htmlFor="rememberMe">Nhớ tôi 30 ngày</label>
-                    </div>
-                    <a href="/forgot-password">Quên mật khẩu</a>
+                <div className={styles.rememberWrapper}>
+                    <input type="checkbox" id="rememberMe" />
+                    <label htmlFor="rememberMe">Nhớ tôi 30 ngày</label>
                 </div>
 
                 <button type="submit" className={styles.loginButton}>Đăng nhập</button>
             </form>
 
-            <div className={styles.signup}>
+            <div className={styles.signupWrapper}>
                 <p>Chưa có tài khoản? <a href="/signup">Sign Up</a></p>
             </div>
         </div>
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,8 +1,30 @@
 import Image from "next/image";
-import styles from "@/app/page.module.css";
+import styles from "./page.module.css";
 
 export default function LoginPage() {
     return (
-        <div>signup</div>
+        <div className={styles.loginWrapper}>
+            <form id={styles.loginForm}>
+                <label>Email</label>
+                <input type="email" placeholder="Nhập email" className={styles.inputField} />
+
+                <div className={styles.passwordTitleWrapper}>
+                    <label>Password</label>
+                    <a href="/forgot-password">Quên mật khẩu</a>
+                </div>
+                <input type="password" placeholder="Mật khẩu" className={styles.inputField} />
+
+                <div className={styles.rememberWrapper}>
+                    <input type="checkbox" id="rememberMe" />
+                    <label htmlFor="rememberMe">Nhớ tôi 30 ngày</label>
+                </div>
+
+                <button type="submit" className={styles.loginButton}>Đăng nhập</button>
+            </form>
+
+            <div className={styles.signupWrapper}>
+                <p>Chưa có tài khoản? <a href="/signup">Sign Up</a></p>
+            </div>
+        </div>
     );
 }
