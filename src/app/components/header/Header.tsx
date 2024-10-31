7f40277241904ffe5fee13e1b9b0e3ba14e14ef1
FoxeiZ <foxeiz2004@gmail.com>
2024-10-31 11:11:21 +0700

    move "Staff" to sidebar instead

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -7,33 +7,32 @@ import { Icon } from '../ui/icon/icon';
 import Image from 'next/image';
 import { usePathname } from 'next/navigation';
 
-export default function Header () {
+export default function Header() {
   const path = usePathname()
   return (
     <div className={styles.container}>
       <div className={styles.logo}>Phan Cafe</div>
       <div className={styles.search}>
-      <Search />
+        <Search />
       </div>
 
       <div className={styles.right}>
-      <nav className="">
-        <Link href={'/employee'} className={path=='/employee' ? styles.navItemActive : styles.navItem } >Nhân viên</Link>
-        <Link href={'/supplier'} className={path=='/supplier' ? styles.navItemActive : styles.navItem }>Nhà cung cấp</Link>
-        <Link href={'/customer'} className={path=='/customer' ? styles.navItemActive : styles.navItem }>Khách hàng</Link>
-        <Link href={'/'} className={styles.navItem}>Hỗ trợ</Link>
-      </nav>
-      <div className={styles.profile}>
-        <Icon icon={
-          <Image src={'/images/notifications.png'} alt='Notification' width={20} height={20}/>
-        }
-        count={3}
-        />
-         <Icon icon={
-          <Image src={'/images/account_circle.png'} alt='Avatar' width={30} height={30}/>
-        }
-        />
-      </div>
+        <nav className="">
+          <Link href={'/supplier'} className={path == '/supplier' ? styles.navItemActive : styles.navItem}>Nhà cung cấp</Link>
+          <Link href={'/customer'} className={path == '/customer' ? styles.navItemActive : styles.navItem}>Khách hàng</Link>
+          <Link href={'/'} className={styles.navItem}>Hỗ trợ</Link>
+        </nav>
+        <div className={styles.profile}>
+          <Icon icon={
+            <Image src={'/images/notifications.png'} alt='Notification' width={20} height={20} />
+          }
+            count={3}
+          />
+          <Icon icon={
+            <Image src={'/images/account_circle.png'} alt='Avatar' width={30} height={30} />
+          }
+          />
+        </div>
       </div>
 
     </div>
--- src/app/components/sidebar/Sidebar.tsx
+++ src/app/components/sidebar/Sidebar.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import styles from './Sidebar.module.scss'
-import React, { ReactNode} from 'react'
+import React, { ReactNode } from 'react'
 import Image from 'next/image'
 import { BiHomeAlt } from "react-icons/bi";
 import { BsInbox } from "react-icons/bs";
@@ -10,61 +10,73 @@ import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 
 
-export default function Sidebar () {
+export default function Sidebar() {
 
   return (
     <div className={styles.container}>
-        <SideBarItem
+      <SideBarItem
         icon={<Image src={'/images/sidebar/home.png'}
-        width={20} height={20}
-        alt='Trang chủ'
-        /> }
+          width={20} height={20}
+          alt='Trang chủ'
+        />}
         iconHover={<Image src={'/images/sidebar/home-hover.png'}
-        width={20} height={20}
-        alt='Trang chủ'
+          width={20} height={20}
+          alt='Trang chủ'
         />
         }
         label='Trang chủ'
-        href='/'/>
-         <SideBarItem
+        href='/' />
+      <SideBarItem
         icon={<Image src={'/images/sidebar/warehouse.png'}
-        width={20} height={20}
-        alt='Kho hàng'
-        /> }
+          width={20} height={20}
+          alt='Kho hàng'
+        />}
         iconHover={<Image src={'/images/sidebar/warehouse-hover.png'}
-        width={20} height={20}
-        alt='Kho hàng'
+          width={20} height={20}
+          alt='Kho hàng'
         />
         }
         label='Kho hàng'
-        href='/warehouse'/>
-         <SideBarItem
+        href='/warehouse' />
+      <SideBarItem
         icon={<Image src={'/images/sidebar/statistic.png'}
-        width={20} height={20}
-        alt='Bán hàng'
-        /> }
+          width={20} height={20}
+          alt='Bán hàng'
+        />}
         iconHover={<Image src={'/images/sidebar/statistic-hover.png'}
-        width={20} height={20}
-        alt='Bán hàng'
+          width={20} height={20}
+          alt='Bán hàng'
         />
         }
         label='Bán hàng'
-        href='/sell'/>
+        href='/sell' />
+      <SideBarItem
+        icon={<Image src={'/images/sidebar/staff.png'}
+          width={20} height={20}
+          alt='Nhân viên'
+        />}
+        iconHover={<Image src={'/images/sidebar/staff-hover.png'}
+          width={20} height={20}
+          alt='Nhân viên'
+        />
+        }
+        label='Nhân viên'
+        href='/sell' />
     </div>
   )
 }
 
 interface SideBarItemProps {
-  icon : ReactNode,
+  icon: ReactNode,
   iconHover: ReactNode,
   label: string,
   href: string,
 }
 
-export function SideBarItem ({icon, iconHover, label,href}: SideBarItemProps) {
+export function SideBarItem({ icon, iconHover, label, href }: SideBarItemProps) {
   const path = usePathname()
   return (
-    <Link className={path==href ? styles.itemActive : styles.item} href={href} >
+    <Link className={path == href ? styles.itemActive : styles.item} href={href} >
       <div className={styles.icon}>{icon}</div> <div className={styles.iconHover}>{iconHover}</div> {label}
     </Link>
   )
