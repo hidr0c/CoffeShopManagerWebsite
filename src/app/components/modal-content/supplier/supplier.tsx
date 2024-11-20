d3a48159f1fb9b5a7d18887c11d486a77ef7f071
FoxeiZ <foxeiz2004@gmail.com>
2024-11-21 05:03:44 +0700

    change formData

--- src/app/components/modal-content/supplier/supplier.tsx
+++ src/app/components/modal-content/supplier/supplier.tsx
@@ -7,7 +7,7 @@ import { Button } from "@ui/button/button";
 // Define the types for the props
 interface SupplierFormProps {
   formData: {
-    supplierName: string;
+    name: string;
     field: string;
     phone: string;
     address: string;
@@ -16,38 +16,27 @@ interface SupplierFormProps {
 }
 
 export default function SupplierForm({ formData, onChange }: SupplierFormProps) {
-  const { supplierName, field, phone, address } = formData;
-
-  // Using the props directly instead of local state
-  const handleSelect = (value: string) => {
-    onChange('field', value); // Update field using onChange
-  };
-
   return (
     <div style={{ margin: '0 0 2em 0' }}>
       <Input
         label='Tên nhà cung cấp'
-        value={supplierName}
-        onChange={(e) => onChange('supplierName', e.target.value)} // Update supplierName using onChange
+        value={formData.name}
+        onChange={(e) => onChange('name', e.target.value)}
       />
-      <Select
+      <Input
         label="Lĩnh vực"
-        value={field}
-        options={[
-          { value: 'Thiết bị điện tử', label: 'Thiết bị điện tử' },
-          { value: 'Trang phục', label: 'Trang phục' }
-        ]}
-        onChange={(e) => handleSelect(e.target.value)} // Handle select field change
+        value={formData.field}
+        onChange={(e) => onChange('field', e.target.value)}
       />
       <Input
         label='SĐT'
-        value={phone}
-        onChange={(e) => onChange('phone', e.target.value)} // Update phone using onChange
+        value={formData.phone}
+        onChange={(e) => onChange('phone', e.target.value)}
       />
       <Input
         label='Địa chỉ'
-        value={address}
-        onChange={(e) => onChange('address', e.target.value)} // Update address using onChange
+        value={formData.address}
+        onChange={(e) => onChange('address', e.target.value)}
       />
     </div>
   );
