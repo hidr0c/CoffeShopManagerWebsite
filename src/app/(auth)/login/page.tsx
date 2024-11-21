e5d0bdf4ff51592e918c9c7e3c544c0b16ca53af
FoxeiZ <foxeiz2004@gmail.com>
2024-11-21 08:16:43 +0700

    move to dashboard if already login

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -8,10 +8,18 @@ import { Input, Checkbox, message } from "antd"
 import AuthApi from "@services/auth";
 import { LoginParams } from "../../models/AuthModel";
 import AuthLayout from "@/components/auth/layout";
+import { useEffect } from "react";
 
 
 export default function LoginPage() {
     const router = useRouter();
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            router.push("/dashboard/");
+        }
+    }, []);
 
     const onSubmit = async (data: LoginParams) => {
         console.log(data);
